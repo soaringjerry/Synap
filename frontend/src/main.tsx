e0e50c78d35a20af74328629aed3fff9757f94af
soaringjerry <soaringjerry@gmail.com>
2025-09-12 17:28:00 +0000

    feat(frontend i18n): integrate i18next/react-i18next; add en/zh resources; translate all UI strings; persist language; reload items by lang

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './styles/global.css'
+import { initI18n } from './i18n'
+
+initI18n()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
